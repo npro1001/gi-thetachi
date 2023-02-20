6eae8961d251a683c897c221a7a61bcddf65ea36
npro1001 <nprocacciojr@gmail.com>
2023-02-19 20:49:04 -0500

    use react seperation

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from get_database import get_database
 import time
 import redis
 import json
+import os
 
 
 app = Flask(__name__, static_folder='./gi-thetachi-expo-react/web-build/') # static_url_path='/gi-thetachi-expo-react')
@@ -23,16 +24,16 @@ def index():
 
 @app.route('/static/js/<path:path>')
 def send_js(path):
-    return send_from_directory(app.static_folder + 'static/js', path)
+    return send_from_directory(os.path.join(app.static_folder, 'static', 'js'), path)
 
 @app.route('/static/js/')
 def send_js_dir():
-    return send_from_directory(app.static_folder + 'static/js', '')
+    return send_from_directory(os.path.join(app.static_folder, 'static', 'js'), '')
 
 # Serve the entire static directory
 @app.route('/static/<path:path>')
 def send_static(path):
-    return send_from_directory(app.static_folder + 'static', path)
+    return send_from_directory(os.path.join(app.static_folder, 'static'), path)
 
 @app.route('/<path:path>')
 def serve_static(path):
