d7d87fe93a971d54d57c015f836b8902b4e33d13
npro1001 <nprocacciojr@gmail.com>
2023-02-19 19:32:39 -0500

    render'

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import redis
 import json
 
 
-app = Flask(__name__, template_folder='./gi-thetachi-expo-react/web-build/')
+app = Flask(__name__, template_folder='.')
 CORS(app)
 app.register_blueprint(sse, url_prefix='/stream')
 
