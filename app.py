2924976f788ed4ccb5c637618102f620c440b095
npro1001 <nprocacciojr@gmail.com>
2023-02-19 20:39:24 -0500

    use react seperation

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import redis
 import json
 
 
-app = Flask(__name__, static_folder='static') # static_url_path='/gi-thetachi-expo-react')
+app = Flask(__name__, static_folder='./gi-thetachi-expo-react/web-build/static') # static_url_path='/gi-thetachi-expo-react')
 CORS(app)
 app.register_blueprint(sse, url_prefix='/stream')
 
@@ -23,20 +23,20 @@ def index():
 
 @app.route('/static/js/<path:path>')
 def send_js(path):
-    return send_from_directory('static/js', path)
+    return send_from_directory(app.static_folder + 'js', path)
 
 @app.route('/static/js/')
 def send_js_dir():
-    return send_from_directory('static/js', '')
+    return send_from_directory(app.static_folder + 'js', '')
 
 # Serve the entire static directory
 @app.route('/static/<path:path>')
 def send_static(path):
-    return send_from_directory('static', path)
+    return send_from_directory(app.static_folder, path)
 
 @app.route('/<path:path>')
 def serve_static(path):
-    return send_from_directory('static', path)
+    return send_from_directory(app.static_folder, path)
 
 
 @app.route('/stream-data')
