f48fdc3ad4a8c66eabf3fffea74eefde83041f89
npro1001 <nprocacciojr@gmail.com>
2023-02-25 19:30:31 -0500

    "Create React App transfer from Expo"

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ port = int(os.environ.get('PORT', 5000)) # default to port 5000 if PORT environm
 
 
 
-app = Flask(__name__, static_folder='./gi-thetachi-expo-react/web-build/')
+app = Flask(__name__, static_folder='./gi-thetachi-create-react-app/release/')
 CORS(app)
 app.register_blueprint(sse, url_prefix='/stream')
 
@@ -34,22 +34,31 @@ def stream_data():
 def index():
     return send_from_directory(app.static_folder, 'index.html')
 
-@app.route('/static/js/<path:path>')
-def send_js(path):
-    return send_from_directory(os.path.join(app.static_folder, 'static', 'js'), path)
+# @app.route('/static/js/<path:path>')
+# def send_js(path):
+#     return send_from_directory(os.path.join(app.static_folder, 'static', 'js'), path)
 
-@app.route('/static/js/')
-def send_js_dir():
-    return send_from_directory(os.path.join(app.static_folder, 'static', 'js'), '')
+# @app.route('/static/js/')
+# def send_js_dir():
+#     return send_from_directory(os.path.join(app.static_folder, 'static', 'js'), '')
 
-# Serve the entire static directory
-@app.route('/static/<path:path>')
-def send_static(path):
-    return send_from_directory(os.path.join(app.static_folder, 'static'), path)
+# # Serve the entire static directory
+# @app.route('/static/<path:path>')
+# def send_static(path):
+#     return send_from_directory(os.path.join(app.static_folder, 'static'), path)
+
+# @app.route('/<path:path>')
+# def serve_static(path):
+#     return send_from_directory(app.static_folder, path)
 
 @app.route('/<path:path>')
 def serve_static(path):
-    return send_from_directory(app.static_folder, path)
+    if path != '' and os.path.exists(app.static_folder + '/' + path):
+        # Serve any static files requested
+        return send_from_directory(app.static_folder, path)
+    else:
+        # Serve index.html for any unknown paths
+        return send_from_directory(app.static_folder, 'index.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=port)
