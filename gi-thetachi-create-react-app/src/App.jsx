8f1eb7b561886f09f78c733cdc6384c4d4b2b9ea
npro1001 <nprocacciojr@gmail.com>
2023-03-05 17:24:52 -0500

    Mobile View completed

--- gi-thetachi-create-react-app/src/App.jsx
+++ gi-thetachi-create-react-app/src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useLayoutEffect} from 'react';
-import {Text, View, Image, Dimensions, TouchableOpacity} from 'react-native';
+import {Text, View, Image, Dimensions, TouchableOpacity, ScrollView, SafeAreaView} from 'react-native';
 import styles from './styles.jsx'
+import styles2 from './styles2.jsx'
 import Animated, { useSharedValue, withTiming, useAnimatedProps, useDerivedValue} from 'react-native-reanimated';
 import Svg, { Circle } from 'react-native-svg';
 import OXLogo from './assets/theta-chi-logo.png';
@@ -12,10 +13,14 @@ const apiUrl = process.env.REACT_APP_SERVER;
 
 Animated.addWhitelistedNativeProps({ text: true });
 
+
+
 const BACKGROUND_COLOR = '#191716';
 const BACKGROUND_STROKE_COLOR = '#78171A';
 const STROKE_COLOR = '#DB4348';
-const { width, height } = Dimensions.get('window');
+// const { width, height } = Dimensions.get('window');
+// const isSmallScreen = (() => { if(width < 500) return True});
+// const isLargeScreen = (() => { if(width >= 800) return True});
 const CIRCLE_LENGTH = 1000; // 2PI*R
 const R = CIRCLE_LENGTH / (2 * Math.PI);
 const AnimatedCircle = Animated.createAnimatedComponent(Circle);
@@ -27,10 +32,18 @@ export default function App() {
   const [progressValue, setProgressValue] = useState(0.0);
   const [progressPercentText, setProgressPercentText] = useState('0%');
   const progressPercent = useSharedValue(0);
+  const [screenWidth, setScreenWidth] = useState(Dimensions.get('window').width);
+  const [screenHeight, setScreenHeight] = useState(Dimensions.get('window').height);
+
   
   useEffect(() => {
     const source = new EventSource(apiUrl + '/stream-data');
-    console.log(process.env.REACT_APP_SERVER);
+
+    const updateLayout = () => {
+      setScreenWidth(Dimensions.get('window').width);
+      setScreenHeight(Dimensions.get('window').height);
+    };
+    Dimensions.addEventListener('change', updateLayout);
 
 
     // Retrieve Server Sent Event
@@ -49,6 +62,7 @@ export default function App() {
     
     return () => {
       source.close();
+      Dimensions.removeEventListener('change', updateLayout);
     };
   })
 
@@ -64,75 +78,132 @@ export default function App() {
     setProgressPercentText(`${percent}%`);
   });
 
-  return (
-    <View style={styles.windowContainer}>
-      {/* *** HEADER *** */}
-      <View style={styles.header}>
-        <Image source={OXLogo} style={styles.OXlogo}/>
-      </View>
-      <View style={styles.body}>
-        {/* *** LEFT SECTION *** */}
-        <View style={styles.leftSection}>
+  {
+    if(screenWidth >= 800) { return (
+      <View style={styles.windowContainer}>
+        {/* *** HEADER *** */}
+        <View style={styles.header}>
+          <Image source={OXLogo} style={styles.OXlogo}/>
+        </View>
+        <View style={styles.body}>
+          {/* *** LEFT SECTION *** */}
+          <View style={styles.leftSection}>
 
-          {/* Goal Text */}
-          <View style={styles.aboveProgressBox}>
-            <Text style={styles.miniText}>Our 2023 Goal</Text>
-            <GoalText goal={goalS}/>
+            {/* Goal Text */}
+            <View style={styles.aboveProgressBox}>
+              <Text style={styles.miniText}>Our 2023 Goal</Text>
+              <Text style={styles.goalText}>${goalS}</Text>
+            </View>
+
+            {/* Progress Circle */}
+            <View style={styles.progressBox}>
+              <Animated.Text style={styles.percentText}>{progressPercentText}</Animated.Text>
+              <Svg style={styles.circle}>
+                <Circle
+                  cx={screenWidth / 2}
+                  cy={screenHeight / 2}
+                  r={R}
+                  fill='none'
+                  stroke={BACKGROUND_STROKE_COLOR}
+                  strokeWidth={30}
+                />
+                <AnimatedCircle
+                  cx={screenWidth / 2}
+                  cy={screenHeight / 2}
+                  r={R}
+                  fill='none'
+                  stroke={STROKE_COLOR}
+                  strokeWidth={15}
+                  strokeDasharray={CIRCLE_LENGTH}
+                  animatedProps={animatedProps}
+                  strokeLinecap={'round'}
+                />
+              </Svg>
+            </View>
+
+            {/* Current Progress */}
+            <Text style={styles.progressText}>Raised ${progressValue}</Text>       
           </View>
 
+          {/* *** RIGHT SECTION *** */}
+          <View style={styles.rightSection}>
+            <Image source={Backdrop} style={{width: '100%', height: '100%', resizeMode: 'cover'}}/>
+            <View style={styles.paragraphContainer}>
+              <Text style={styles.titleText}>G.I. Theta Chi 2023</Text>
+              <Image source={USOLogo} style={styles.USOlogo}/>
+              <Text style={styles.paragraph}>In 2013 the first ever G.I. Theta Chi event was held by the UCF chapter of Theta Chi.  Since then, it has grown into one of the largest philanthropic events hosted by various chapters  across the nation. Since it’s conception, we have raised over $100,000 for the U.S.O. and  raised over $45,000 last year alone.  </Text>
+              <Text style={styles.paragraph}>The U.S.O. strengthens America’s military service members by keeping them connected to family, home and country throughout their service to the nation.</Text>
+              <Text style={styles.paragraph}>Help us hit our 2023 goal!    <span style={{fontWeight:'bold', textDecoration:'underline'}}>Venmo: @githetachi23</span></Text>
+            </View>
+          </View>
+        </View>
+      </View>
+      ); 
+  } else { return (
+    <View style={styles2.mobileContainer}>
+      {/* <ScrollView style={styles2.scrollView} contentContainerStyle={styles2.contentContainer}> */}
+        <View style={styles2.container1}>
+          {/* OX Logo */}
+          <View style={styles2.header}>
+            <Image source={OXLogo} style={styles2.OXlogo}/>
+          </View>
+          {/* Goal Text */}
+          <View style={styles2.aboveProgressBox}>
+            <Text style={styles2.miniText}>Our 2023 Goal</Text>
+            <Text style={styles2.goalText}>${goalS}</Text>
+                        {/* Current Progress */}
+                        <ProgressText style={styles2.progressText} value={progressValue}/>
+          </View>
           {/* Progress Circle */}
-          <View style={styles.progressBox}>
-            <Animated.Text style={styles.percentText}>{progressPercentText}</Animated.Text>
-            <Svg style={styles.circle}>
+          <View style={styles2.progressBox}>
+            <Animated.Text style={styles2.percentText}>{progressPercentText}</Animated.Text>
+            <Svg style={styles2.circle}>
+              <Text>HI</Text>
               <Circle
-                cx={width / 2}
-                cy={height / 2}
-                r={R}
+                cx={screenWidth / 2}
+                cy={screenHeight / 2}
+                r={screenWidth / (2 * Math.PI)}
                 fill='none'
                 stroke={BACKGROUND_STROKE_COLOR}
-                strokeWidth={30}
+                strokeWidth={15}
               />
               <AnimatedCircle
-                cx={width / 2}
-                cy={height / 2}
-                r={R}
+                cx={screenWidth / 2}
+                cy={screenHeight / 2}
+                r={screenWidth / (2 * Math.PI)}
                 fill='none'
                 stroke={STROKE_COLOR}
-                strokeWidth={15}
+                strokeWidth={7}
                 strokeDasharray={CIRCLE_LENGTH}
                 animatedProps={animatedProps}
                 strokeLinecap={'round'}
               />
             </Svg>
           </View>
-
-          {/* Current Progress */}
-          <ProgressText style={styles.progressText} value={progressValue}/>       
         </View>
-
-        {/* *** RIGHT SECTION *** */}
-        <View style={styles.rightSection}>
+        <View style={styles2.container2}>   
           <Image source={Backdrop} style={{width: '100%', height: '100%', resizeMode: 'cover'}}/>
-          <View style={styles.paragraphContainer}>
-            <Text style={styles.titleText}>G.I. Theta Chi 2023</Text>
-            <Image source={USOLogo} style={styles.USOlogo}/>
-            <Text style={styles.paragraph}>In 2013 the first ever G.I. Theta Chi event was held by the UCF chapter of Theta Chi.  Since then, it has grown into one of the largest philanthropic events hosted by various chapters  across the nation. Since it’s conception, we have raised over $100,000 for the U.S.O. and  raised over $45,000 last year alone.  </Text>
-            <Text style={styles.paragraph}>The U.S.O. strengthens America’s military service members by keeping them connected to family, home and country throughout their service to the nation.</Text>
-            <Text style={styles.paragraph}>Help us hit our 2023 goal!    <span style={{fontWeight:'bold', textDecoration:'underline'}}>Venmo: @githetachi23</span></Text>
+          <View style={styles2.paragraphContainer}>
+            <Text style={styles2.titleText}>G.I. Theta Chi 2023</Text>
+            <Image source={USOLogo} style={styles2.USOlogo}/>
+            <Text style={styles2.paragraph}>In 2013 the first ever G.I. Theta Chi event was held by the UCF chapter of Theta Chi.  Since then, it has grown into one of the largest philanthropic events hosted by various chapters  across the nation. Since it’s conception, we have raised over $100,000 for the U.S.O. and  raised over $45,000 last year alone.  </Text>
+            <Text style={styles2.paragraph}>The U.S.O. strengthens America’s military service members by keeping them connected to family, home and country throughout their service to the nation.</Text>
+            <Text style={styles2.paragraph}>Help us hit our 2023 goal!</Text>
+            <Text style={styles2.paragraph}><span style={{fontWeight:'bold', textDecoration:'underline'}}>Venmo: @githetachi23</span></Text>
           </View>
         </View>
-      </View>
+    {/* </ScrollView> */}
     </View>
-    
-  );
+  );}}
+
 }
 
 
 const ProgressText = ({ value }) => {
-  return <Text style={styles.progressText}>Raised ${value}</Text>
+  return <Text>Raised ${value}</Text>
 }
 
 const GoalText = ({ goal }) => {
-  return <Text style={styles.goalText}>${goal}</Text>
+  return <Text>${goal}</Text>
 }
 
---
+++ gi-thetachi-create-react-app/src/styles2.jsx
@@ -0,0 +1,181 @@
+import {StyleSheet, Dimensions} from 'react-native';
+const BACKGROUND_COLOR = '#fff'; //'#191716';
+const BACKGROUND_STROKE_COLOR = '#78171A';
+const TEXT_COLOR = '#191716';
+const TEXT_COLOR2 = '#fff';
+const TEXT_COLOR3 = '#09274B';
+
+const STROKE_COLOR = '#DB4348';
+const { width, height } = Dimensions.get('window');
+const CIRCLE_LENGTH = 300; // 2PI*R
+const R = CIRCLE_LENGTH / (2 * Math.PI);
+import Backdrop from './assets/custom-background.png'
+
+const styles2 = StyleSheet.create({
+    scrollView: {
+        flex: 1,
+        height: '100%',
+        // alignItems: 'center',
+        // justifyContent: 'center',
+        // backgroundColor: '#fff',
+    },
+    contentContainer: {
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: '#fff',
+    },
+    mobileContainer: {
+        flex: 1,
+        flexDirection: 'column',
+        backgroundColor: BACKGROUND_COLOR,
+        // alignItems: 'center',
+        justifyContent: 'center',
+        height: height + 500,
+        width: width,
+        // overflowX: "hidden",
+        overflowY: "scroll",
+        overflow: 'hidden',
+        // overflow: "scroll",
+        flexGrow: 1,
+    },
+    container2: {
+        flex: 1,
+        zIndex: 2,
+        // flexGrow: 2,
+        // overflowY: "scroll",
+        position: 'relative',
+        // height: height,
+        height: height + 500,
+    },
+    container1: {
+        flex: 0.5,
+        // flexGrow: 1,
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    header: {
+        zIndex: 1,
+        backgroundColor: BACKGROUND_COLOR,
+        // flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+        position: 'absolute',
+        width: width,
+        height: height/10,
+        top: 30,
+        left: 0,
+        marginBottom: height/10,
+        flex: 1
+    },
+    OXlogo: {
+        zIndex: 2,
+        width: "100px",
+        height: "100px",
+        aspectRatio: 1,
+        resizeMode: 'contain',
+      },
+    circle: {
+        zIndex: 1,
+        flex: 1,
+        position: 'absolute',
+        height: height,
+        width: width,
+    },
+    aboveProgressBox: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: BACKGROUND_COLOR,
+        position: 'relative',
+        textAlign: 'center',
+        marginTop: height/10
+    },
+    progressBox: {
+        zIndex: 2,
+        flex: 1,
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: BACKGROUND_COLOR,
+        aspectRatio: 1,
+        paddingBottom: 30,
+    },
+    progressText: {
+        flex: 1,
+        fontSize: 40,
+        color: TEXT_COLOR3,
+        marginTop: 20,
+        position: 'absolute',
+        textAlign: 'center',
+    },
+    rightSection: {
+        zIndex: 1,
+        flex: 2,
+        backgroundColor: BACKGROUND_COLOR,
+        backgroundImage: `url(${Backdrop})`,
+        backgroundSize: "fill",
+        flexDirection: 'column',
+        resizeMode: 'contain',
+        backgroundRepeat: "no-repeat",
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: width * 0.66,
+        height: '100%',
+    },
+      USOlogo: {
+        width: '20%',
+        height: '30%',
+        resizeMode: 'contain',
+        aspectRatio: 1.5,
+      },
+      textContainer: {
+        position: 'absolute',
+        top: height * 0.1,
+        width: '100%',
+        alignItems: 'center',
+      },
+      percentText: {
+        fontSize: 40,
+        color: TEXT_COLOR,
+        textAlign: 'center',
+      },
+      titleText: {
+        fontSize: 40,
+        color: TEXT_COLOR2,
+        textAlign: 'center',
+        padding: '2%'
+      },
+      goalText: {
+        fontSize: 40,
+        color: TEXT_COLOR3,
+        textAlign: 'center',
+        fontWeight: 'bold',
+        padding: '5%',
+        // borderRadius: 25,
+      },
+      paragraphContainer: {
+        position: 'absolute',
+        // flexDirection: 'column',
+        top: height * 0.1,
+        width: '100%',
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: '5%',
+        fontSize: 10,
+      },
+      paragraph: {
+        fontSize: 20,
+        color: TEXT_COLOR2,
+        textAlign: 'center',
+        padding: '2%'
+      },
+      miniText: {
+        fontSize: 20,
+        color: TEXT_COLOR3,
+        textAlign: 'center',
+        fontStyle: 'italic',
+      }
+    });
+    
+    export default styles2;
